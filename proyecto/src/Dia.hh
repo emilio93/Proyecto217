225312df4aec76951bdf93cf972f7be513093c69
Emilio Rojas <emilio93@gmail.com>
2015-10-30 12:26:37 -0600

    Agrego enum Dia

---
+++ proyecto/src/Dia.hh
@@ -0,0 +1,19 @@
+#ifndef ENUM_DIA
+#define ENUM_DIA
+
+/**
+ * @breif Días de la semana e indefinido que representa cualquier día.
+ *        De esta manera, indefinido no de puede comparar con los otros días.
+ */
+enum Dia {
+    LUNES       = 1,
+    MARTES      = 2,
+    MIERCOLES   = 3,
+    JUEVES      = 4,
+    VIERNES     = 5,
+    SABADO      = 6,
+    DOMINGO     = 7,
+    INDEFINIDO  = 0
+};
+
+#endif ENUM_DIA
