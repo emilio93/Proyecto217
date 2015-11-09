4fca19fb08038ced15674c74bc049d97c7ae6ae2
Emilio Rojas <emilio93@gmail.com>
2015-11-09 14:14:49 -0600

    A curso falta implementar un método y agregarle otro

--- proyecto/generador/src/Curso.cpp
+++ proyecto/generador/src/Curso.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
 #include <stddef.h>
 #include <string>
-#include <list>
+#include <vector>
 
+#include "Bloque.hh"
 #include "Curso.hh"
 #include "Grupo.hh"
 #include "Misc.hh"
@@ -11,48 +11,95 @@
  ** MÉTODOS PÚBLICOS
  ******************************************************************************/
 
-Curso::Curso(int id, int cantidadHoras, int clasesEnSemana, int cantidadGrupos, std::string nombre, std::string sigla) {
+Curso::Curso(int id, int cantidadHoras, int clasesEnSemana, int cantidadGrupos,
+    Bloque *bloque, std::string nombre, std::string sigla) {
     this->setId(id);
     this->setCantidadHoras(cantidadHoras);
     this->setClasesEnSemana(clasesEnSemana);
     this->setCantidadGrupos(cantidadGrupos);
+    if (bloque != NULL) {
+        this->setBloques(new std::vector<Bloque*>());
+        this->getBloques()->push_back(bloque);
+    }
     this->setNombre(nombre);
     this->setSigla(sigla);
 }
 
-int Curso::getId(void) { return this->id; }
+int Curso::getId(void) {
+    return this->id;
+}
 
-void Curso::setId(int id) { this->id = id; }
+void Curso::setId(int id) {
+    this->id = id;
+}
 
-int Curso::getCantidadHoras(void) { return this->cantidadHoras; }
+int Curso::getCantidadHoras(void) {
+    return this->cantidadHoras;
+}
 
-void Curso::setCantidadHoras(int cantidadHoras) { this->cantidadHoras = cantidadHoras; }
+void Curso::setCantidadHoras(int cantidadHoras) {
+    this->cantidadHoras = cantidadHoras;
+}
 
-int Curso::getClasesEnSemana(void) { return this->clasesEnSemana; }
+int Curso::getClasesEnSemana(void) {
+    return this->clasesEnSemana;
+}
 
-void Curso::setClasesEnSemana(int clasesEnSemana) { this->clasesEnSemana = clasesEnSemana; }
+void Curso::setClasesEnSemana(int clasesEnSemana) {
+    this->clasesEnSemana = clasesEnSemana;
+}
 
-int Curso::getCantidadGrupos(void) { return this->cantidadGrupos; }
+int Curso::getCantidadGrupos(void) {
+    return this->cantidadGrupos;
+}
 
-void Curso::setCantidadGrupos(int cantidadGrupos) { this->cantidadGrupos = cantidadGrupos; }
+void Curso::setCantidadGrupos(int cantidadGrupos) {
+    this->cantidadGrupos = cantidadGrupos;
+}
 
-std::string Curso::getNombre(void) { return this->nombre; }
+std::vector<Bloque*> *Curso::getBloques(void) {
+    return this->bloques;
+}
 
-void Curso::setNombre(std::string nombre) { this->nombre = nombre; }
+void Curso::setBloques(std::vector<Bloque*> *bloques) {
+    this->bloques = bloques;
+}
 
-std::string Curso::getSigla(void) { return this->sigla; }
+std::string Curso::getNombre(void) {
+    return this->nombre;
+}
 
-void Curso::setSigla(std::string sigla) { this->sigla = sigla; }
+void Curso::setNombre(std::string nombre) {
+    this->nombre = nombre;
+}
+
+std::string Curso::getSigla(void) {
+    return this->sigla;
+}
+
+void Curso::setSigla(std::string sigla) {
+    this->sigla = sigla;
+}
 
 std::string Curso::toString(void) {
     return this->getNombre() + " - " + this->getSigla();
 }
 
-std::list<Grupo*> * Curso::crearGrupos(void) {
-    std::list<Grupo*> * grupos = new std::list<Grupo*>();
+std::vector<Grupo*> *Curso::crearGrupos(void) {
+    std::vector<Grupo*> *grupos = new std::vector<Grupo*>();
+    //código
+    //TODO
+    //TODO
+    //TODO
     //código
     return grupos;
 }
+
+bool Curso::igual(Curso *curso) {
+    return this->getSigla().compare(curso->getSigla()) == 0 &&
+           this->getNombre().compare(curso->getNombre()) == 0;
+}
+
 
  /*******************************************************************************
   ** MÉTODOS PRIVADOS
@@ -65,13 +112,52 @@ std::list<Grupo*> * Curso::crearGrupos(void) {
  * @return  0.
  */
 int testCurso(void) {
+    using namespace std;
+    Curso *curso1 = new Curso(1, 5, 2, 1, NULL, "Circuitos Lineles I", "IE0209");
+    cout << curso1->toString() << " creado." << endl;
+    Curso *curso2 = new Curso(1, 5, 2, 1, NULL, "Circuitos Lineles I", "IE0209");
+    cout << curso2->toString() << " creado." << endl;
+    Curso *curso3 = new Curso(2, 5, 2, 1, NULL, "Circuitos Lineles II", "IE0309");
+    cout << curso3->toString() << " creado." << endl;
 
-    Curso * curso1 = new Curso(1, 5, 2, 1, "Circuitos Lineles I", "IE0209");
-    std::cout << curso1->toString() << " creado." << std::endl;
-    std::cout << "\tid: " << curso1->getId() << std::endl;
-    std::cout << "\tcantidadHoras: " << curso1->getCantidadHoras() << std::endl;
-    std::cout << "\tclasesEnSemana: " << curso1->getClasesEnSemana() << std::endl;
-    std::cout << "\tCantidadGrupos: " << curso1->getCantidadGrupos() << std::endl;
+    cout << "Curso1:" << endl;
+    cout << "\tid: " << curso1->getId() << endl;
+    cout << "\t" << curso1->toString() << endl;
+    cout << "\tcantidadHoras: " << curso1->getCantidadHoras() << endl;
+    cout << "\tclasesEnSemana: " << curso1->getClasesEnSemana() << endl;
+    cout << "\tCantidadGrupos: " << curso1->getCantidadGrupos() << endl << endl;
+
+    cout << "Curso2:" << endl;
+    cout << "\tid: " << curso2->getId() << endl;
+    cout << "\t" << curso2->toString() << endl;
+    cout << "\tcantidadHoras: " << curso2->getCantidadHoras() << endl;
+    cout << "\tclasesEnSemana: " << curso2->getClasesEnSemana() << endl;
+    cout << "\tCantidadGrupos: " << curso2->getCantidadGrupos() << endl << endl;
+
+    cout << "Curso3:" << endl;
+    cout << "\tid: " << curso3->getId() << endl;
+    cout << "\t" << curso3->toString() << endl;
+    cout << "\tcantidadHoras: " << curso3->getCantidadHoras() << endl;
+    cout << "\tclasesEnSemana: " << curso3->getClasesEnSemana() << endl;
+    cout << "\tCantidadGrupos: " << curso3->getCantidadGrupos() << endl << endl;
+
+    if (curso1->igual(curso2)) {
+        cout << "Curso1 es igual a Curso2." << endl;
+    } else {
+        cout << "Curso1 no es igual a Curso2." << endl;
+    }
+
+    if (curso1->igual(curso3)) {
+        cout << "Curso1 es igual a Curso3." << endl;
+    } else {
+        cout << "Curso1 no es igual a Curso3." << endl;
+    }
+
+    if (curso2->igual(curso3)) {
+        cout << "Curso2 es igual a Curso3." << endl;
+    } else {
+        cout << "Curso2 no es igual a Curso3." << endl;
+    }
 
     return 0;
 }
