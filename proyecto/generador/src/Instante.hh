9b8566bbddca6bb11eb35e63b140f6da3af920a0
Emilio Rojas <emilio93@gmail.com>
2015-11-11 20:45:51 -0600

    Corrigo documentacion en Instante

--- proyecto/generador/src/Instante.hh
+++ proyecto/generador/src/Instante.hh
@@ -5,104 +5,123 @@
 #include "IInstante.hh"
 #include "Dia.hh"
 
-
-/**
- * @breif La clase Instante representa un instante en una semana. Los días se
- *        definen en el enum DIA, la horas están en el rango [0,23], los minutos
- *        en [0,59].
+//! Un instante dentro de una semana.
+/*!
+ * La clase Instante representa un instante en una semana. Tiene alcanze de dias,
+ * horas y minutos. Los días se definen en el enum DIA, la horas están en el rango
+ * [0,23], los minutos en [0,59].
  */
 class Instante : public IInstante
 {
     private:
-        /**
-         * @breif El día del instante según el ENUM DIA.
+        //! El día del instante.
+        /*!
+         * El día del instante según el ENUM DIA.
          */
         Dia dia;
 
-        /**
-         * @breif La hora del instante. En caso que esté fuera del rango [0,23]
-         *        se cambiará el día según corresponda. Si el día es indefinido,
-         *        este no cambiará.
+        //! La Hora del instante.
+        /*!
+         * La hora del instante. En caso que esté fuera del rango [0,23]
+         * se cambiará el día según corresponda. Si el día es indefinido,
+         * este no cambiará.
          */
         int hora;
 
-        /**
-         * @breif El minuto del instante. En caso que este fuera del rango
-         *        [0,59] se cambiará la hora según corresponda.
-         *        Para nuestro caso, los minutos serán 0 en todo caso.
+        //! El minuto del instante.
+        /*!
+         * El minuto del instante. En caso que este fuera del rango
+         * [0,59] se cambiará la hora según corresponda.
+         * Para nuestro caso, los minutos serán 0 en todo caso.
          */
         int minuto;
 
-        /**
-         * @breif Chequea que el instante sea adecuado segun los rangos
-         *        especificados para los minutos y las horas.
-         * @return  True si es uin tiempo que cumple las restricciones.
+        //! Chequea que el instante esté en formato correcto.
+        /*!
+         * Chequea que el instante sea adecuado segun los rangos
+         * especificados para los minutos y las horas.
+         * @return True si es uin tiempo que cumple las restricciones.
          */
         bool chequearRestricciones(void);
 
-        /**
-         * @breif Devuelve un instante corregido a partir de otro instante,
-         *        asegurandose que cumpla las resticciones de los rangos de
-         *        dias, horas y minutos.
-         *        En caso que el día no corresponda a ninguno, se asignará como
-         *        INDEFINIDO.
-         * @param  instante Instante que se desea corregir
-         * @return          Un Instante dentro de los rangos adecuados.
+        //! Devuelve un instante equivalente con formato correcto.
+        /*!
+         * Devuelve un instante corregido a partir de otro instante,
+         * asegurandose que cumpla las resticciones de los rangos de
+         * dias, horas y minutos.
+         * En caso que el día no corresponda a ninguno, se asignará como
+         * INDEFINIDO.
+         * @param instante Instante que se desea corregir
+         * @return Un Instante dentro de los rangos adecuados.
          */
-        Instante * getInstante(IInstante * instante);
+        Instante * getInstante(IInstante *instante);
 
-        /**
-         * @breif Corrige el instante para que cumpla los rangos de días, horas
-         *        y minutos.
-         *        En caso que el día no corresponda a ninguno, se asignará como
-         *        INDEFINIDO.
+        //! Devuelve un instante en formato correcto.
+        /*!
+         * Corrige el instante para que cumpla los rangos de días, horas y minutos.
+         * En caso que el día no corresponda a ninguno, se asignará como INDEFINIDO.
          */
         void getInstante(void);
 
     public:
-
         virtual std::string toString(void);
-        virtual bool igual(IInstante * instante);
-        virtual bool posterior(IInstante * instante);
-        virtual bool previo(IInstante * instante);
+        virtual bool igual(IInstante *instante);
+        virtual bool posterior(IInstante *instante);
+        virtual bool previo(IInstante *instante);
 
-        /**
-         * @breif Constructor de la clase. Inicializa los valores.
+        //! Asigna un instante.
+        /*!
+         * Constructor de la clase. Inicializa los valores para el día, la hora y el
+         * minuto.
          */
         Instante(Dia dia = INDEFINIDO, int hora = 0, int minuto = 0);
 
-        /**
-         * @breif Método get para el día del instante.
+        //! Convierte un IInstante a Instante.
+        /*!
+         * Obtiene a partir de un IInstante un Instante.
+         * @param  instante El IInstante a convertir.
+         * @return El Instante convertido.
+         */
+        static Instante *toInstante(IInstante *instante);
+
+        //! Obtiene el día del instante.
+        /*!
+         * Método get para el día del instante.
          * @return El día del instante.
          */
         Dia getDia(void);
 
-        /**
-         * @breif Método set para el día del instante.
+        //! Asigna el día del instante.
+        /*!
+         * Método set para el día del instante.
          * @param dia El nuevo día del instante.
          */
         void setDia(Dia dia);
 
-        /**
-         * @breif Método get para la hora del instante.
+        //! Obtiene la hora del día.
+        /*!
+         * Método get para la hora del instante.
          * @return La hora del instante.
          */
         int getHora(void);
 
-        /**
-         * @breif Método set para la hora del instante.
+        //! Asigna la hora del instante.
+        /*!
+         * Método set para la hora del instante.
          * @param hora La nueva hora del instante.
          */
         void setHora(int hora);
 
-        /**
-         * @breif Método get para el minuto del instante.
+        //! Obtiene el minuto del instante.
+        /*!
+         * Método get para el minuto del instante.
          * @return El minuto del instante.
          */
         int getMinuto(void);
 
-        /**
-         * @breif Método set para el minuto del instante.
+        //! Asigna el minuto del instante.
+        /*!
+         * Método set para el minuto del instante.
          * @param minuto El nuevo minuto del instante.
          */
         void setMinuto(int minuto);
