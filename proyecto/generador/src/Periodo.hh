6ec198010b1146f78f1f286039df38c27ad5bcce
Emilio Rojas <emilio93@gmail.com>
2015-11-11 20:07:40 -0600

    Corrigo documentacion

--- proyecto/generador/src/Periodo.hh
+++ proyecto/generador/src/Periodo.hh
@@ -1,64 +1,72 @@
 #ifndef CLASE_PERIODO
 #define CLASE_PERIODO
 
+#include "IPeriodo.hh"
+#include "IInstante.hh"
+
 //! Un período delimitado por instantes inicial y final.
-/**
+/*!
  * La clase Periodo define un periodo delimitado dentro de una semana.
  * Se define según un Instante inicial y otro final. En caso de que se
  * utilize días indefinidos, deben estar en el inicio y en el fin.
  */
 class Periodo : public IPeriodo {
     private:
-        /**
-         * @breif El inicio del Periodo.
+        //! Inicio del periodo.
+        /*!
+         * El inicio del periodo.
          */
-        Instante *inicio;
+        IInstante *inicio;
 
-        /**
-         * @breif El fin del Periodo.
+        //! El fin del periodo.
+        /*!
+         * El fin del periodo.
          */
-        Instante *fin;
+        IInstante *fin;
+
+        //! Obtiene la duración en horas del Periodo.
+        /*!
+         * Obtiene la duración en horas del Periodo. En caso de que el periodo dure
+         * varios días, se obtiene igualmente las horas que transcurren del instante
+         * inicial al final.
+         * @return  La duración en horas del Periodo.
+         */
+        int getDuracion(void);
 
     public:
-        /**
-         * @breif Constructor de la clase.
+        //! El constructor de la clase.
+        /*!
+         * Asigna los valores de instante inicial e intante final respectivamente.
+         * @param inicio Instante inicial.
+         * @param fin Instante final.
          */
-        Periodo(Instante *inicio, Instante *fin);
+        Periodo(IInstante *inicio, IInstante *fin);
 
-        /**
-         * @breif Método set para el Instante inicial del Periodo.
-         * @param Instante El nuevo incio del Periodo.
+        //! Método set para el Instante inicial del Periodo.
+        /*!
+         * Método set para el Instante inicial del Periodo.
+         * @param instante El nuevo incio del Periodo.
          */
-        void setInicio(Instante *instante);
+        void setInicio(IInstante *instante);
 
-        /**
-         * @breif Método set para el Instante final del Periodo.
-         * @param Instante El nuevo fin del Periodo.
+        //! Método set para el Instante final del Periodo.
+        /*!
+         * Método set para el Instante final del Periodo.
+         * @param instante El nuevo fin del Periodo.
          */
-        void setFin(Instante *instante);
+        void setFin(IInstante *instante);
 
         virtual IInstante *getInicio(void);
-
-        virtual IInstante *getFinal(void);
-
+        virtual IInstante *getFin(void);
         virtual bool igual(IPeriodo *periodo);
-
         virtual bool contieneInclusivo(IPeriodo *periodo);
-
         virtual bool contieneExclusivo(IPeriodo *periodo);
-
         virtual bool enInclusivo(IPeriodo *periodo);
-
         virtual bool enExclusivo(IPeriodo *periodo);
-
         virtual bool igualDuracion(IPeriodo *periodo);
-
         virtual bool mayorDuracion(IPeriodo *periodo);
-
         virtual bool menorDuracion(IPeriodo *periodo);
-
         virtual bool traslapa(IPeriodo *periodo);
-
 };
 
 #endif
