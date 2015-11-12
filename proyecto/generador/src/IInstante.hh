694873c0f17adc623b62c0ea2e66e4f18f95317c
Emilio Rojas <emilio93@gmail.com>
2015-11-11 20:47:24 -0600

    Corrigo documentacion en IInstante

--- proyecto/generador/src/IInstante.hh
+++ proyecto/generador/src/IInstante.hh
@@ -3,9 +3,8 @@
 
 #include <string>
 
-/**
- * @breif Representación de un instante.
- *
+//! Representación de un instante.
+/*!
  * Este instante comprende un punto en el sistema temporal en que se trabaje.
  * Contiene operaciones de comparación así como un método para representarse
  * mediante un string.
@@ -14,29 +13,36 @@ class IInstante {
     public:
         virtual ~IInstante(void) {;}
 
-        /**
-         * @breif Esta es la manera en que el instante se representa mediante un
-         *        string característico a si.
-         * @return  String qe representa e instante.
+        //! Obtiene texto representativo del instante.
+        /*!
+         * Esta es la manera en que el instante se representa mediante un
+         * string característico a si.
+         * @return  String que representa e instante.
          */
         virtual std::string toString(void) = 0;
 
-        /**
-         * @breif Indica si dos instantes coinciden en el tiempo.
+        //! Indica si dos instantes coinciden en el tiempo.
+        /*!
+         * Indica si dos instantes coinciden en el tiempo.
+         * @param instante Instante con el que se compara.
          */
-        virtual bool igual(IInstante * instante) = 0;
+        virtual bool igual(IInstante *instante) = 0;
 
-        /**
-         * @breif Indica si el instante es posterior al instante con
-         *        el cual se compara.
+        //! Indica si el instante es posterior a otro.
+        /*!
+         * Indica si el instante es posterior al instante con
+         * el cual se compara.
+         * @param instante Instante con el que se compara.
          */
-        virtual bool posterior(IInstante * instante) = 0;
+        virtual bool posterior(IInstante *instante) = 0;
 
-        /**
-         * @breif Indica si el instante es previo al instante con el
-         *        cual se compara.
+        //! Indica si el instante es previo a otro.
+        /*!
+         * Indica si el instante es previo al instante con el
+         * cual se compara.
+         * @param instante Instante con el que se compara.
          */
-        virtual bool previo(IInstante * instante) = 0;
+        virtual bool previo(IInstante *instante) = 0;
 };
 
 #endif
