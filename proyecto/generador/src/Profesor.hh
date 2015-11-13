b1091cf7fd0a9b9678dbed2784f4622c0daf7213
juan199 <juancho0_8@hotmail.com>
2015-11-13 15:43:22 -0600

    profesor.hh

--- proyecto/generador/src/Profesor.hh
+++ proyecto/generador/src/Profesor.hh
@@ -9,6 +9,7 @@
 #include "IPeriodo.hh"
 
 class Curso;
+class Grupo;
 
 
 //#using namespace std;
@@ -25,14 +26,61 @@ class Profesor {
 
 private:
 
-	int id, horasLaborales, horasAsignadas;
-	std::string nombre, apellido; //esto no se porque está aquí xD que buscaban con esto?
+//! private int id
+/*!
+ * Contiene el id asignado al profesor pos la base de datos
+ */
+	int id; 
+	
+//! private int horasLaborales
+/*!
+ * Número constante de horas laborales para el profesor
+ */	
+	int horasLaborales;
+
+
+//! private int horasAsignadas
+/*!
+ * Horas Asignadas de cursos a profesor
+ */	
+	int horasAsignadas;
+
+
+//! private string nombre
+/*!
+ * Nombre del profesor
+ */	
+	std::string nombre;
+	
+//! private string nombre
+/*!
+ * Apellido del profesor
+ */	 
+	std::string apellido; 
+	
+	
 public:
 
 
-	std::priority_queue<Curso> * cursos;//no se si los vamos a usar pero ahi están
+	std::priority_queue<Curso> * cursos;//No sé si los vamos a usar pero ahi están
+	
 	//std::priority_queue<IPeriodo> * disponibilidad;
-	int getHorario(Profesor * Profesoractual);// esto debe devolver el horario del profe
+	
+	int getHorario(Profesor * Profesoractual);// Esto debe devolver el horario del profe
+	
+//! Método público bool verificar_curso
+/*!
+ * Devuelve un 1 si el profesor puede dar el curso
+ */	
+	bool verificar_curso(Curso * curso_prueba);
+
+
+//! Método público asignar_grupo
+/*!
+ * Modifica el horario del profesor y horasAsignadas
+ */	
+	void asignar_grupo(Grupo * Grupo); /*podria devolver un horario untilizando la función getHorario()*/
+	
 };
 
 #endif
