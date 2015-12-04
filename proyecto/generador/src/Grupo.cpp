5673c0ae8c0f3fe368a0f2b02cde6b8a0012fa87
Emilio Rojas <emilio93@gmail.com>
2015-12-04 16:35:31 -0600

    Estilación de Grupo

--- proyecto/generador/src/Grupo.cpp
+++ proyecto/generador/src/Grupo.cpp
@@ -29,7 +29,6 @@ int Grupo::getId(void) {
 
 void Grupo::setId(int id) {
     this->id = id;
-
 }
 
 Curso *Grupo::getCurso(void) {
@@ -38,7 +37,6 @@ Curso *Grupo::getCurso(void) {
 
 void Grupo::setCurso(Curso *curso) {
     this->curso = curso;
-
 }
 
 Profesor *Grupo::getProfesor(void) {
@@ -47,16 +45,14 @@ Profesor *Grupo::getProfesor(void) {
 
 void Grupo::setProfesor(Profesor *profesor) {
     this->profesor = profesor;
-
 }
 
 std::vector<IPeriodo*> *Grupo::getHorario() {
     return this->horario;
 }
 
-void Grupo::setHorario(std::vector<IPeriodo*> *horario){
+void Grupo::setHorario(std::vector<IPeriodo*> *horario) {
     this->horario = horario;
-
 }
 
 std::vector<Profesor> *Grupo::getPosiblesProfesores(void) {
@@ -70,23 +66,22 @@ std::vector<Profesor> *Grupo::getPosiblesProfesores(void) {
 bool Grupo::asignarProfesor(void) {
     bool asignado = false;
     std::vector<Profesor> *profesores = this->getPosiblesProfesores();
-	size_t i = 0;
+    size_t i = 0;
     for (i = 0; i < profesores->size(); i++) {
-		//se busca un profesor disponible para este grupo
+        // se busca un profesor disponible para este grupo
         if (profesores->at(i).estoyDisponible(this->getHorario())) {
             this->setProfesor(&profesores->at(i));
             asignado = true;
             break;
         }
-
     }
 
     if (!asignado) {
-
-		//std::vector<int>::iterator it = ; //iterador no me sirve
-		grupo_sin_profesor->insert(grupo_sin_profesor->begin() + contador,this->getCurso());
-		//inserta el puntero al curso de este grupo a la lista
-		contador ++;
+        // std::vector<int>::iterator it = ;  // iterador no me sirve
+        grupo_sin_profesor->insert(grupo_sin_profesor->begin() + contador,
+            this->getCurso());
+        // inserta el puntero al curso de este grupo a la lista
+        contador++;
     }
     return asignado;
 }
