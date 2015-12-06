ea4c4da8760814946b1dab352beee665d5c8bb0c
Emilio Rojas <emilio93@gmail.com>
2015-12-06 17:15:48 -0600

    Nuevos datos

---
+++ proyecto/sql/profesor.sql
@@ -0,0 +1,9 @@
+--
+-- Dumping data for table `Profesor`
+--
+
+INSERT INTO `Profesor` (`horasLaborales`, `nombre`, `apellido`) VALUES
+(36, 'Teodoro', 'Willink'),
+(40, 'Francisco', 'Siles'),
+(30, 'Sergio', 'Valverde'),
+(48, 'Geovanny', 'Martinez');
---
+++ proyecto/sql/profesoresCurso.sql
@@ -0,0 +1,9 @@
+--
+-- Dumping data for table `ProfesoresCurso`
+--
+
+INSERT INTO `ProfesoresCurso` (`idCurso`, `idProfesor`, `prioridad`) VALUES
+(3, 1, 0),
+(5, 2, 0),
+(3, 3, 0),
+(3, 4, 0);
