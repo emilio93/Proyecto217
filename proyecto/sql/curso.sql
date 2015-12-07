687377d1547ee533862ae96dad6ce4f17f2e3290
MarcoAMontero <marco_montero1991@hotmail.com>
2015-12-07 17:24:52 -0600

    Update curso.sql

--- proyecto/sql/curso.sql
+++ proyecto/sql/curso.sql
@@ -6,13 +6,20 @@ INSERT INTO `Curso` (`cantidadHoras`, `clasesEnSemana`, `cantidadGrupos`, `nombr
 (4, 2, 3, 'Circuitos Lineales I', 'IE0209'),
 (4, 2, 3, 'Circuitos Lineales II', 'IE0309'),
 (5, 2, 3, 'Matemáticas Superiores', 'IE0305'),
-(6, 2, 3, 'Programación bajo Plataformas Abiertas', 'IE0117'),
-(6, 2, 2, 'Estructuras Abstractas de Datos', 'IE0217'),
+(6, 2, 2, 'Programación bajo Plataformas Abiertas', 'IE0117'),
+(6, 2, 1, 'Estructuras Abstractas de Datos', 'IE0217'),
 (4, 2, 3, 'Electrónica I', 'IE0313'),
 (4, 2, 2, 'Circuitos Digitales I', 'IE0323'),
 (4, 2, 2, 'Modelos Probabilísticos de Señales y Sistemas', 'IE0405'),
 (4, 2, 2, 'Teoría del Campo I', 'IE0307'),
-(4, 2, 2, 'Laboratorio Eléctrico I', 'IE0308'),
-(4, 2, 2, 'Electrónica II', 'IE0413');
-
+(6, 2, 1, 'Laboratorio Eléctrico I', 'IE0308'),
+(4, 2, 2, 'Electrónica II', 'IE0413'),,  
+(4, 2, 3, 'Circuitos Digitales II', 'IE0309'),
+(4, 2, 1, 'Dispositivos Semiconductores', 'IE0311'),
+(4, 2, 2, 'Análisis de Sistemas', 'IE0409'),
+(4, 2, 2, 'Estructuras de Computadoras Digitales I', 'IE0321'),
+(4, 2, 1, 'Estructuras de Computadoras Digitales II', 'IE0521'),
+(6, 2, 2, 'Máquinas Eléctricas I','IE0315'),
+(4, 2, 2, 'Sistemas de Control', 'IE0431');
+(6, 2, 1, 'Laboratorio Eléctrico I', 'IE0408'),
 -- --------------------------------------------------------
